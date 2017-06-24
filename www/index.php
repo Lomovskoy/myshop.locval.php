466858bf3ea1639e77a5ca5452517939f5dcb6eb
Lomovskoy <lomovskoy.kirill@yandex.ru>
2017-06-24 11:43:26 +0300

    Формируем мия подключаемого файла и название контроллера по средствам гет запроса

--- www/index.php
+++ www/index.php
@@ -1,4 +1,13 @@
 <?php
-    phpinfo();
+    //Определение контроллера для работы
+    $controllerName = isset($_GET['controller']) ? ucfirst($_GET['controller']):'Index';
+    $actionName = isset($_GET['action']) ? $_GET['action']:'index';
+    echo 'Подключаемый php файл (Контроллер) = ' . $controllerName . '<br/>';
+    echo 'Функция формирующая страницу (Экшн) = ' . $actionName . '<br/>';
+    //Подключаем контроллер
+    include_once '../controllers/'.$controllerName.'Controller.php';
+    //Формируем название функции
+    $function = $actionName . 'Action';
+    echo 'Полсное название вызываемой функции = '.$function;
 ?>
 
